4050946d67d33c4149ae0a87ef9ecaea206173e4
Your Name <you@example.com>
2025-12-02 22:34:34 +0800

    12.2 mass revision on the 2 main files

--- TheadPool.cpp
+++ TheadPool.cpp
@@ -7,5 +7,44 @@
 #include <future>
 #include <atomic>
 #include <string>
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(size_t numofthread):stop(false){
+    for(int i = 0; i<numofthread;i++){
+        workers.emplace_back([this]{
+            this->workerThread();
+        });
+    }
+}
+
+ThreadPool::~ThreadPool(){
+{
+    std::unique_lock<std::mutex> lock();
+    stop = true;
+}
+
+for(std::thread &worker:workers){
+    if(worker.joinable()){
+        worker.join();
+    }
+}
 
 
+}
+void ThreadPool::workerThread(){
+    while(true){
+        std::function<void()> task;
+        {
+            std::unique_lock<std::mutex> lock(queuemutex);
+            if(stop && workqueue.empty()){
+                return;
+            }
+            condition.wait(lock,[this]{
+                return stop || !workqueue.empty();
+            });
+            task = std::move(workqueue.front());  
+            workqueue.pop();
+        }
+        task();
+    }
+}
--- ThreadPool.h
+++ ThreadPool.h
@@ -11,5 +11,30 @@
 #include <atomic>
 #include <string>
 
+class ThreadPool{
+    public:
+    ThreadPool(size_t numofthread);
+    ~ThreadPool();
+
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+
+    void workerThread();
+
+    private:
+    std::vector<std::thread> workers;   //工作线程
+    std::queue<std::function<void()>> workqueue;    //任务队列
+
+    mutable std::mutex queuemutex;
+    std::condition_variable condition;
+    std::atomic<bool> stop; //停止标志 用于析构
+};
+
+
+
+
+
+
+
 
 #endif
